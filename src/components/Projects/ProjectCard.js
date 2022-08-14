e0dd32ed99538f85fa08d935447eb87a09d63609
[Kevin Jordan] <kevinjordan199449@gmail.com>
2022-08-14 10:20:04 -0400

    Create basic template for project card

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -1,6 +1,6 @@
 import Card from "../../ui/Card";
 import styles from "./ProjectCard.module.css";
-import image from "../../images/smart-macro.PNG";
+import image from "../../icons/macros.png";
 
 export default function ProjectCard() {
   return (
@@ -11,6 +11,18 @@ export default function ProjectCard() {
           <div className={styles.project_image}>
             <img src={image} alt="smart macro"></img>
           </div>
+          <p className={styles.project_description}>
+            Smart macro is a meal planning application that utilizes the
+            Spoonacular API to allow health-conscious users to find meals based
+            on their macronutrient needs.
+          </p>
+          <a
+            className={styles.project_link}
+            href="https://kevinjordan1994.github.io/smart-macro/"
+            target="break"
+          >
+            Check It Out!
+          </a>
         </div>
       </Card>
     </div>
